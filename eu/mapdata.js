e223de1690ffc44dae843d97d318a2a24edee66f
Maja <136361838+majaszafranska@users.noreply.github.com>
2026-06-26 15:18:23 +0200

    EU map

---
+++ eu/mapdata.js
@@ -0,0 +1,355 @@
+var simplemaps_europemap_mapdata={
+  main_settings: {
+   //General settings
+    width: "responsive", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    popups: "detect",
+    
+    //State defaults
+    state_description: "",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+    //Location defaults
+    location_description: "x",
+    location_color: "#8b5fbf",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_url: "",
+    location_size: "10",
+    location_type: "circle",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 1,
+    location_hover_border: 1.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    manual_zoom: "no",
+    back_image: "no",
+    arrow_color: "#cecece",
+    arrow_color_border: "#808080",
+    initial_back: "no",
+    initial_zoom: -1,
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    AL: {
+      name: "Albania",
+      zoomable: "no"
+    },
+    AM: {
+      name: "Armenia",
+      zoomable: "no"
+    },
+    AT: {
+      name: "Austria",
+      zoomable: "no"
+    },
+    BA: {
+      name: "Bośnia i Hercegowina",
+      zoomable: "no"
+    },
+    BE: {
+      name: "Belgia",
+      zoomable: "no"
+    },
+    BG: {
+      name: "Bułgaria",
+      zoomable: "no"
+    },
+    BY: {
+      name: "Białoruś",
+      zoomable: "no"
+    },
+    CH: {
+      name: "Szwajcaria",
+      zoomable: "no"
+    },
+    CY: {
+      name: "Cypr",
+      zoomable: "no"
+    },
+    CZ: {
+      name: "Czechy",
+      zoomable: "no"
+    },
+    DE: {
+      name: "Niemcy",
+      color: "#ffd166",
+      hover_color: "#f4a261",
+      description: "grudzień 2025"
+    },
+    DK: {
+      name: "Dania",
+      zoomable: "no"
+    },
+    EE: {
+      name: "Estonia",
+      zoomable: "no"
+    },
+    ES: {
+      name: "Hiszpania",
+      zoomable: "no"
+    },
+    FI: {
+      name: "Finlandia",
+      zoomable: "no"
+    },
+    FR: {
+      name: "Francja",
+      zoomable: "no"
+    },
+    GB: {
+      name: "Wielka Brytania",
+      color: "#ffd166",
+      hover_color: "#f4a261",
+      description: "marzec 2025"
+    },
+    GE: {
+      name: "Gruzja",
+      zoomable: "no"
+    },
+    GR: {
+      name: "Grecja",
+      zoomable: "no"
+    },
+    HR: {
+      name: "Chorwacja",
+      zoomable: "no"
+    },
+    HU: {
+      name: "Węgry",
+      zoomable: "no"
+    },
+    IE: {
+      name: "Irlandia",
+      zoomable: "no"
+    },
+    IS: {
+      name: "Islandia",
+      zoomable: "no"
+    },
+    IT: {
+      name: "Włochy",
+      zoomable: "no"
+    },
+    LT: {
+      name: "Litwa",
+      zoomable: "no"
+    },
+    LU: {
+      name: "Luksembourg",
+      zoomable: "no"
+    },
+    LV: {
+      name: "Łotwa",
+      zoomable: "no"
+    },
+    MD: {
+      name: "Mołdawia",
+      zoomable: "no"
+    },
+    ME: {
+      name: "Montenegro",
+      zoomable: "no"
+    },
+    MK: {
+      name: "Macedonia",
+      zoomable: "no"
+    },
+    NL: {
+      name: "Holandia",
+      zoomable: "no"
+    },
+    NO: {
+      name: "Norwegia",
+      zoomable: "no"
+    },
+    PL: {
+      name: "Polska",
+      color: "#ffd166",
+      hover_color: "#f4a261",
+      description: "2024 - 2026"
+    },
+    PT: {
+      name: "Portugalia",
+      zoomable: "no"
+    },
+    RO: {
+      name: "Rumunia",
+      zoomable: "no"
+    },
+    RS: {
+      name: "Serbia",
+      zoomable: "no"
+    },
+    SE: {
+      name: "Szwecja",
+      zoomable: "no"
+    },
+    SI: {
+      name: "Słowenia",
+      zoomable: "no"
+    },
+    SK: {
+      name: "Słowacja",
+      zoomable: "no"
+    },
+    TR: {
+      name: "Turcja",
+      color: "#ffd166",
+      hover_color: "#f4a261",
+      description: "wrzesień 2025"
+    },
+    UA: {
+      name: "Ukraina",
+      zoomable: "no"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "51.5072",
+      lng: "-0.1275",
+      name: "Londyn",
+      description: "Musicale ❀"
+    },
+    "1": {
+      name: "Berlin",
+      lat: "52.5200",
+      lng: "13.4050",
+      description: "Mikołajki ❄︎"
+    },
+    "2": {
+      lat: "36.8549",
+      lng: "28.2742",
+      name: "Marmaris",
+      description: "Wakacje '25 ☄"
+    },
+    "3": {
+      lat: "52.2297",
+      lng: "21.0122",
+      name: "Warszawa",
+      description: "Domek ♡"
+    },
+    "4": {
+      name: "Szczecin",
+      description: "Koncert ♪",
+      lat: "53.4285",
+      lng: "14.5528"
+    },
+    "5": {
+      lat: "53.1235",
+      lng: "18.0084",
+      description: "Rodzina ♡",
+      name: "Bydgoszcz"
+    },
+    "6": {
+      lat: "54.3520",
+      lng: "18.6466",
+      name: "Gdańsk",
+      description: "Wakacje '24 i zima '25 ⛱︎"
+    },
+    "7": {
+      lat: "53.524",
+      lng: "16.309",
+      name: "Czaplinek",
+      description: "Woodstock ☮"
+    },
+    "8": {
+      lat: "52.004",
+      lng: "23.534",
+      name: "Podolanka",
+      description: "Rocznica ①"
+    },
+    "9": {
+      name: "Toruń",
+      lat: "53.0138",
+      lng: "18.5984",
+      description: "Copernicon ♖"
+    },
+    "10": {
+      lat: "54.0359",
+      lng: "19.0266",
+      name: "Malbork",
+      description: "Prawie-majówka ♔"
+    },
+    "11": {
+      lng: "19.6022",
+      lat: "51.2011",
+      name: "Plucice",
+      description: "Rodzina ♡"
+    },
+    "12": {
+      name: "Opoczno",
+      lat: "51.376",
+      lng: "20.288",
+      description: "Wesele ⚣"
+    },
+    "13": {
+      lat: "54.5189",
+      lng: "18.5305",
+      name: "Gdynia",
+      description: "Wakacje '24 ⛱︎"
+    },
+    "14": {
+      lat: "54.4416",
+      lng: "18.5601",
+      name: "Sopot",
+      description: "Wakacje '24 ⛱︎"
+    },
+    "15": {
+      lat: "49.4330",
+      lng: "20.3020",
+      name: "Krośnica",
+      description: "Larpowe urodziny 🂡"
+    },
+    "16": {
+      lat: "52.7015",
+      lng: "23.8386",
+      name: "Białowieża",
+      description: "Rocznica ②"
+    }
+  },
+  regions: {},
+  labels: {},
+  legend: {
+    entries: []
+  }
+};
